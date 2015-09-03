f46afe286ce3b782bb74ef604fc2bf94d8a5c790
Agustin Lago <lago_agustin@hotmail.com>
2015-09-03 01:23:18 -0300

    Modificacion del Cliente/Servidor y Agregada funcion para recepcion/envio de instrucciones
    Signed-off-by: Agustin Lago <lago_agustin@hotmail.com>
    
    Signed-off-by: Agustin Lago <lago_agustin@hotmail.com>

--- CPU/src/CPU.c
+++ CPU/src/CPU.c
@@ -1,6 +1,6 @@
 /*
  ============================================================================
- Name        : Planificador.c
+ Name        : CPU.c
  Author      : Game of Pointers
  Version     :
  Copyright   : Your copyright notice
@@ -15,18 +15,20 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define IP "127.0.0.1"
 #define PUERTO "8080"
+#define PACKAGESIZE 1024
 
 //Inicio de funcion principal
-int main(void)
+int main()
 {
-	puts("!!!Planificador!!"); /* prints !!!Planificador!! */
+	puts("!!!CPU!!"); /* prints !!!CPU!! */
 
 	//Configuracion del socket
-	struct addrinfo hints; //estructura que almacena los datos de conexion del planificador
-	struct addrinfo *serverInfo; //estructura que almacena los datos de conexion de la CPU
+	struct addrinfo hints; //estructura que almacena los datos de conexion de la CPU
+	struct addrinfo *serverInfo; //estructura que almacena los datos de conexion del Planificador
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;		// Permite que la maquina se encargue de verificar si usamos IPv4 o IPv6
@@ -34,21 +36,38 @@ int main(void)
 
 	getaddrinfo(IP, PUERTO, &hints, &serverInfo);	// Carga en serverInfo los datos de la conexion
 
-	//se crea un nuevo socket que se utilizara para la conecxion con la CPU
+	//se crea un nuevo socket que se utilizara para la conexion con el Planificador
 	int cpuSocket; //descriptor del socket servidor
 	cpuSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
 	//se comprueba que el socket se creo correctamente
 	if(cpuSocket==-1)
 		perror ("SOCKET");
 
-	//Conexion al servidor CPU
+	//Conexion al servidor Planificador
 	int C = connect(cpuSocket, serverInfo->ai_addr, serverInfo->ai_addrlen);
 	if (C==-1)
 		perror ("CONNECT");
 	else
-		printf ("Conexion con la CPU lograda\n");
+		printf ("Conexion con el Planificador lograda\n");
 
 	freeaddrinfo(serverInfo);
+
+//Recepcion de instrucciones
+
+
+	char package[PACKAGESIZE];
+		int status = 1;		// Estructura que manjea el status de los recieve.
+
+		printf("CPU conectada. Esperando instrucciones:\n");
+
+		while (status != 0){
+			status = recv(cpuSocket, (void*) package, PACKAGESIZE, 0);
+			if (status != 0) printf("%s", package);
+
+		}
+
+
+
 
 	close(cpuSocket);
 
--- Planificador/src/Planificador.c
+++ Planificador/src/Planificador.c
@@ -1,6 +1,6 @@
 /*
  ============================================================================
- Name        : CPU.c
+ Name        : Planificador.c
  Author      : Game of Pointers
  Version     :
  Copyright   : Your copyright notice
@@ -15,51 +15,93 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define IP "127.0.0.1"
 #define PUERTO "8080"
 #define BACKLOG 10
+#define PACKAGESIZE 1024
 
-int main(void)
-{
-	puts("!!!CPU!!!"); /* prints !!!CPU!!! */
+
+struct param{
+ int socket;
+ int n;
+};
+
+void escuchar (struct param *parametros) {
+
+
+}
+
+
+int main() {
+	puts("!!!Planificador!!!"); /* prints !!!Planificador!!! */
 
 	//Configuracion del socket
-	struct addrinfo hints; //estructura que almacena los datos de conexion de la CPU
-	struct addrinfo *serverInfo; //estructura que almacena los datos de conexion del planificador
+	struct addrinfo hints; //estructura que almacena los datos de conexion del Planificador
+	struct addrinfo *serverInfo; //estructura que almacena los datos de conexion de la CPU
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;		// No importa si uso IPv4 o IPv6
-	hints.ai_flags = AI_PASSIVE;		// Asigna el address del localhost: 127.0.0.1
+	hints.ai_flags = AI_PASSIVE;// Asigna el address del localhost: 127.0.0.1
 	hints.ai_socktype = SOCK_STREAM;	// Indica que usaremos el protocolo TCP
 
-	getaddrinfo(NULL, PUERTO, &hints, &serverInfo);  // Carga en serverInfo los datos de la conexion
+	getaddrinfo(NULL, PUERTO, &hints, &serverInfo); // Carga en serverInfo los datos de la conexion
 
-	//se crea un nuevo socket que se utilizara para la conecxion con el Planificador
+	//se crea un nuevo socket que se utilizara para la conexion con el CPU
 	int listenningSocket;
-	listenningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
+	listenningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype,
+			serverInfo->ai_protocol);
 	//se comprueba que el socket se creo correctamente
-	if (listenningSocket==-1)
-		perror ("SOCKET");
+	if (listenningSocket == -1)
+		perror("SOCKET");
 
 	//se comprueba que la asociacion fue exitosa
-	int B = bind(listenningSocket,serverInfo->ai_addr, serverInfo->ai_addrlen);
-	if (B==-1)
-	    perror("BIND");
+	int B = bind(listenningSocket, serverInfo->ai_addr, serverInfo->ai_addrlen);
+	if (B == -1)
+		perror("BIND");
 
 	freeaddrinfo(serverInfo);
 
+
 	//funcion que permite al programa ponerse a la espera de nuevas conexiones
-	int L= listen (listenningSocket, BACKLOG);
-	if (L==-1)
-	    perror ("LISTEN");
+		int L = listen(listenningSocket, BACKLOG);
+		if (L == -1)
+			perror("LISTEN");
+
+	//Hilo para escucha
+
+/*
+	struct param param1 = {0,0};
+	pthread_t hiloListen;
+	pthread_create (&hiloListen, NULL, (void*)escuchar, (void*)&param1);
+*/
+
+
 
 	//Estructura que tendra los datos de la conexion del cliente
 	struct sockaddr_in addr;
 	socklen_t addrlen = sizeof(addr);
 
-	int socketPlanificador = accept(listenningSocket, (struct sockaddr *) &addr, &addrlen);
-	printf ("Conexion recivida\n");
+	int socketPlanificador = accept(listenningSocket, (struct sockaddr *) &addr,
+			&addrlen);
+	printf("Conexion recibida\n");
+
+	//Envio de instrucciones
+
+	int enviar = 1;
+		char message[PACKAGESIZE];
+
+		printf("Conectado al CPU. Ya puede enviar instrucciones. Escriba 'exit' para finalizar\n");
+
+		while(enviar){
+			fgets(message, PACKAGESIZE, stdin);			// Lee una linea en el stdin (lo que escribimos en la consola) hasta encontrar un \n (y lo incluye) o llegar a PACKAGESIZE.
+			if (!strcmp(message,"exit\n")) enviar = 0;			// Chequeo que el usuario no quiera salir
+			if (enviar) send(socketPlanificador, message, strlen(message) + 1, 0); 	// Solo envio si el usuario no quiere salir.
+		}
+
+		close(socketPlanificador);
+		close(listenningSocket);
 
 	return EXIT_SUCCESS;
 }
