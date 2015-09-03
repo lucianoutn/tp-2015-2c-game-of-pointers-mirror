f3280d49b698de6336ba0ba654b29730d6a9d6ba
Agustin Lago <lago_agustin@hotmail.com>
2015-09-03 00:34:10 -0300

    Modificacion del Cliente/Servidor y Agregada funcion para recepcion/envio de instrucciones
    Signed-off-by: Agustin Lago <lago_agustin@hotmail.com>

--- CPU/src/CPU.c
+++ CPU/src/CPU.c
@@ -1,10 +1,10 @@
 /*
  ============================================================================
- Name        : CPU.c
+ Name        : Planificador.c
  Author      : Game of Pointers
  Version     :
  Copyright   : Your copyright notice
- Description : Modulo - CPU
+ Description : Modulo - Planificador
  ============================================================================
  */
 
@@ -18,48 +18,41 @@
 
 #define IP "127.0.0.1"
 #define PUERTO "8080"
-#define BACKLOG 10
 
+//Inicio de funcion principal
 int main(void)
 {
-	puts("!!!CPU!!!"); /* prints !!!CPU!!! */
+	puts("!!!Planificador!!"); /* prints !!!Planificador!! */
 
 	//Configuracion del socket
-	struct addrinfo hints; //estructura que almacena los datos de conexion de la CPU
-	struct addrinfo *serverInfo; //estructura que almacena los datos de conexion del planificador
+	struct addrinfo hints; //estructura que almacena los datos de conexion del planificador
+	struct addrinfo *serverInfo; //estructura que almacena los datos de conexion de la CPU
 
 	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;		// No importa si uso IPv4 o IPv6
-	hints.ai_flags = AI_PASSIVE;		// Asigna el address del localhost: 127.0.0.1
+	hints.ai_family = AF_UNSPEC;		// Permite que la maquina se encargue de verificar si usamos IPv4 o IPv6
 	hints.ai_socktype = SOCK_STREAM;	// Indica que usaremos el protocolo TCP
 
-	getaddrinfo(NULL, PUERTO, &hints, &serverInfo);  // Carga en serverInfo los datos de la conexion
+	getaddrinfo(IP, PUERTO, &hints, &serverInfo);	// Carga en serverInfo los datos de la conexion
 
-	//se crea un nuevo socket que se utilizara para la conecxion con el Planificador
-	int listenningSocket;
-	listenningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
+	//se crea un nuevo socket que se utilizara para la conecxion con la CPU
+	int cpuSocket; //descriptor del socket servidor
+	cpuSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
 	//se comprueba que el socket se creo correctamente
-	if (listenningSocket==-1)
+	if(cpuSocket==-1)
 		perror ("SOCKET");
 
-	//se comprueba que la asociacion fue exitosa
-	int B = bind(listenningSocket,serverInfo->ai_addr, serverInfo->ai_addrlen);
-	if (B==-1)
-	    perror("BIND");
+	//Conexion al servidor CPU
+	int C = connect(cpuSocket, serverInfo->ai_addr, serverInfo->ai_addrlen);
+	if (C==-1)
+		perror ("CONNECT");
+	else
+		printf ("Conexion con la CPU lograda\n");
 
 	freeaddrinfo(serverInfo);
 
-	//funcion que permite al programa ponerse a la espera de nuevas conexiones
-	int L= listen (listenningSocket, BACKLOG);
-	if (L==-1)
-	    perror ("LISTEN");
+	close(cpuSocket);
 
-	//Estructura que tendra los datos de la conexion del cliente
-	struct sockaddr_in addr;
-	socklen_t addrlen = sizeof(addr);
 
-	int socketPlanificador = accept(listenningSocket, (struct sockaddr *) &addr, &addrlen);
-	printf ("Conexion recivida\n");
 
 	return EXIT_SUCCESS;
 }
--- Planificador/src/Planificador.c
+++ Planificador/src/Planificador.c
@@ -1,10 +1,10 @@
 /*
  ============================================================================
- Name        : Planificador.c
+ Name        : CPU.c
  Author      : Game of Pointers
  Version     :
  Copyright   : Your copyright notice
- Description : Modulo - Planificador
+ Description : Modulo - CPU
  ============================================================================
  */
 
@@ -18,41 +18,48 @@
 
 #define IP "127.0.0.1"
 #define PUERTO "8080"
+#define BACKLOG 10
 
-//Inicio de funcion principal
 int main(void)
 {
-	puts("!!!Planificador!!"); /* prints !!!Planificador!! */
+	puts("!!!CPU!!!"); /* prints !!!CPU!!! */
 
 	//Configuracion del socket
-	struct addrinfo hints; //estructura que almacena los datos de conexion del planificador
-	struct addrinfo *serverInfo; //estructura que almacena los datos de conexion de la CPU
+	struct addrinfo hints; //estructura que almacena los datos de conexion de la CPU
+	struct addrinfo *serverInfo; //estructura que almacena los datos de conexion del planificador
 
 	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;		// Permite que la maquina se encargue de verificar si usamos IPv4 o IPv6
+	hints.ai_family = AF_UNSPEC;		// No importa si uso IPv4 o IPv6
+	hints.ai_flags = AI_PASSIVE;		// Asigna el address del localhost: 127.0.0.1
 	hints.ai_socktype = SOCK_STREAM;	// Indica que usaremos el protocolo TCP
 
-	getaddrinfo(IP, PUERTO, &hints, &serverInfo);	// Carga en serverInfo los datos de la conexion
+	getaddrinfo(NULL, PUERTO, &hints, &serverInfo);  // Carga en serverInfo los datos de la conexion
 
-	//se crea un nuevo socket que se utilizara para la conecxion con la CPU
-	int cpuSocket; //descriptor del socket servidor
-	cpuSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
+	//se crea un nuevo socket que se utilizara para la conecxion con el Planificador
+	int listenningSocket;
+	listenningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
 	//se comprueba que el socket se creo correctamente
-	if(cpuSocket==-1)
+	if (listenningSocket==-1)
 		perror ("SOCKET");
 
-	//Conexion al servidor CPU
-	int C = connect(cpuSocket, serverInfo->ai_addr, serverInfo->ai_addrlen);
-	if (C==-1)
-		perror ("CONNECT");
-	else
-		printf ("Conexion con la CPU lograda\n");
+	//se comprueba que la asociacion fue exitosa
+	int B = bind(listenningSocket,serverInfo->ai_addr, serverInfo->ai_addrlen);
+	if (B==-1)
+	    perror("BIND");
 
 	freeaddrinfo(serverInfo);
 
-	close(cpuSocket);
+	//funcion que permite al programa ponerse a la espera de nuevas conexiones
+	int L= listen (listenningSocket, BACKLOG);
+	if (L==-1)
+	    perror ("LISTEN");
 
+	//Estructura que tendra los datos de la conexion del cliente
+	struct sockaddr_in addr;
+	socklen_t addrlen = sizeof(addr);
 
+	int socketPlanificador = accept(listenningSocket, (struct sockaddr *) &addr, &addrlen);
+	printf ("Conexion recivida\n");
 
 	return EXIT_SUCCESS;
 }
