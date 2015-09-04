7e4e2179d04a37d52db0e6f3db1c667472956556
Luciano Marques <luciano.utn@gmail.com>
2015-09-04 04:25:35 -0300

    Proceso Memoria ahora puede enviar msj a Swap!

--- Memoria/src/Memoria.c
+++ Memoria/src/Memoria.c
@@ -17,9 +17,54 @@
 #include <unistd.h>
 
 #define IP "127.0.0.1"
-#define PUERTO "8080"
+#define PUERTO "9000" //este es el puerto de swap??
+#define BACKLOG 10
+#define PACKAGESIZE 1024
+
 
 int main(void) {
 	puts("!!!Memoria!!!"); /* prints !!!Memoria!!! */
-	return EXIT_SUCCESS;
+
+	//Configuracion del socket
+		struct addrinfo hints; //estructura que almacena los datos de conexion de la Memoria
+		struct addrinfo *serverInfo; //estructura que almacena los datos de conexion del Swap
+
+		memset(&hints, 0, sizeof(hints));
+		hints.ai_family = AF_UNSPEC;		// Permite que la maquina se encargue de verificar si usamos IPv4 o IPv6
+		hints.ai_socktype = SOCK_STREAM;	// Indica que usaremos el protocolo TCP
+
+		getaddrinfo(IP, PUERTO, &hints, &serverInfo);	// Carga en serverInfo los datos de la conexion
+
+	//se crea un nuevo socket que se utilizara para la conexion con el swap
+		int memSocket; //descriptor del socket de la memoria
+		memSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
+	//se comprueba que el socket se creo correctamente
+		if(memSocket==-1)
+			perror ("SOCKET");
+
+	//Conexion al servidor Swap
+		int C = connect(memSocket, serverInfo->ai_addr, serverInfo->ai_addrlen);
+		if (C==-1)
+			perror ("CONNECT");
+		else
+			printf ("Conexion con el Swap lograda\n");
+
+		freeaddrinfo(serverInfo);
+
+	//Envio de instrucciones
+
+		int enviar = 1;
+		char message[PACKAGESIZE];
+		if (C!=1)
+			printf("Conectado al Swap en el Socket=%d. Ya puede enviar instrucciones. Escriba 'exit' para finalizar\n",memSocket);
+
+		while(enviar){
+			fgets(message, PACKAGESIZE, stdin);			// Lee una linea en el stdin (lo que escribimos en la consola) hasta encontrar un \n (y lo incluye) o llegar a PACKAGESIZE.
+			if (!strcmp(message,"exit\n")) enviar = 0;			// Chequeo que el usuario no quiera salir
+			if (enviar) send(memSocket, message, strlen(message) + 1, 0); 	// Solo envio si el usuario no quiere salir.
+		}
+
+
+
+		return EXIT_SUCCESS;
 }
--- Swap/src/Swap.c
+++ Swap/src/Swap.c
@@ -17,9 +17,93 @@
 #include <unistd.h>
 
 #define IP "127.0.0.1"
-#define PUERTO "8080"
+#define PUERTO "9000"
+#define BACKLOG 10
+#define PACKAGESIZE 1024
 
 int main(void) {
 	puts("!!!Swap!!!"); /* prints !!!Swap!!! */
+
+	//Configuracion del socket
+	struct addrinfo hints; //estructura que almacena los datos de conexion del Swap
+	struct addrinfo *serverInfo; //estructura que almacena los datos de conexion de la Memoria
+
+		memset(&hints, 0, sizeof(hints));
+		hints.ai_family = AF_UNSPEC;		// No importa si uso IPv4 o IPv6
+		hints.ai_flags = AI_PASSIVE;// Asigna el address del localhost: 127.0.0.1
+		hints.ai_socktype = SOCK_STREAM;	// Indica que usaremos el protocolo TCP
+
+		getaddrinfo(NULL, PUERTO, &hints, &serverInfo); // Carga en serverInfo los datos de la conexion
+
+
+
+	//se crea un nuevo socket que se utilizara para la escucha de la Memoria
+		int listenningSocket;
+		listenningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype,
+				serverInfo->ai_protocol);
+		//se comprueba que el socket se creo correctamente
+		if (listenningSocket == -1)
+			perror("SOCKET");
+
+		//se comprueba que la asociacion fue exitosa
+		int B = bind(listenningSocket, serverInfo->ai_addr, serverInfo->ai_addrlen);
+		if (B == -1)
+			perror("BIND");
+
+		freeaddrinfo(serverInfo);
+
+		//lo pongo a escuchar
+		//Estructura que tendra los datos de la conexion del cliente MEMORIA
+		struct sockaddr_in addr;
+		socklen_t addrlen = sizeof(addr);
+
+		int L = listen(listenningSocket, BACKLOG);
+		if (L == -1)
+			perror("LISTEN");
+
+		int socketSwap, *nuevo_socket;
+		while( (socketSwap = accept(listenningSocket, (struct sockaddr *) &addr,	&addrlen) ) )
+			{
+				printf("Conexion aceptada Socket= %d \n",socketSwap);
+
+				/*pthread_t hilo;
+				nuevo_socket=malloc(1);
+				*nuevo_socket=socketSwap;
+
+				if(pthread_create(&hilo,NULL,multiplesConexiones,(void*)nuevo_socket)<0)
+				{
+					perror("No se puede crear el hilo");
+					return 1;
+				}
+
+				puts("Nueva conexion asignada");*/
+
+
+				//Recepcion de instrucciones
+
+				char package[PACKAGESIZE];
+				int status = 1;		// Estructura que manjea el status de los recieve.
+				printf("ADM de Memoria conectado. Esperando instrucciones:\n");
+				while (status != 0){
+					status = recv(socketSwap, (void*) package, PACKAGESIZE, 0);
+					if (status != 0) printf("%s", package);
+
+				}
+
+
+
+			  }
+
+				//free(nuevo_socket);
+
+
+
+
+				close(socketSwap);
+				close(listenningSocket);
+
+
+
+
 	return EXIT_SUCCESS;
 }
