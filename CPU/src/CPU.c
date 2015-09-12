1d7972958de96ecdb5888f73aabc36558fa5a2ba
Luciano Marques <luciano.utn@gmail.com>
2015-09-12 01:56:51 -0300

    arreglo un poco el codigo

--- CPU/src/CPU.c
+++ CPU/src/CPU.c
@@ -10,11 +10,6 @@
 
 #include "libreriaCliente.h"
 
-/*
-#define IP "127.0.0.1"
-#define PUERTOPLANIFICADOR "8080"
-#define PUERTOMEMORIA "8090"
-*/
 #define PACKAGESIZE 1024
 
 const char *IP = "127.0.0.1";
@@ -28,57 +23,9 @@ int main()
 {
 	puts("!!!CPU!!"); /* prints !!!CPU!! */
 
-	/*
-	//Configuracion del socket
-	struct addrinfo hints_planificador, hints_memoria; //estructura que almacena los datos de conexion de la CPU
-	struct addrinfo *serverInfo_planificador, *serverInfo_memoria; //estructura que almacena los datos de conexion del Planificador
-*/
-/*
-	memset(&hints_planificador, 0, sizeof(hints_planificador));
-	hints_planificador.ai_family = AF_UNSPEC;		// Permite que la maquina se encargue de verificar si usamos IPv4 o IPv6
-	hints_planificador.ai_socktype = SOCK_STREAM;	// Indica que usaremos el protocolo TCP
-	serverInfo_memoria = serverInfo_planificador;
-	hints_memoria = hints_planificador;
-	getaddrinfo(IP, PUERTOPLANIFICADOR, &hints_planificador, &serverInfo_planificador);	// Carga en serverInfo los datos de la conexion
 
-	//se crea un nuevo socket que se utilizara para la conexion con el Planificador
-	int socketPlanificador, socketMemoria; //descriptor del socket servidor
-	socketPlanificador = socket(serverInfo_planificador->ai_family, serverInfo_planificador->ai_socktype, serverInfo_planificador->ai_protocol);
-	//se comprueba que el socket se creo correctamente
-	if(socketPlanificador==-1)
-		perror ("SOCKET PLANIFICADOR!");
-
-	getaddrinfo(IP_MEMORIA ,PUERTOMEMORIA,&hints_memoria,&serverInfo_memoria);
-
-	socketMemoria = socket(serverInfo_memoria->ai_family, serverInfo_memoria->ai_socktype, serverInfo_memoria->ai_protocol);
-	if(socketMemoria==-1)
-		perror("SOCKET MEMORIA!");
-	*/
-
-/*	//Conexion al servidor Planificador y a Memoria
-	int C = connect(socketPlanificador, serverInfo_planificador->ai_addr, serverInfo_planificador->ai_addrlen);
-	if (C==-1){
-		perror ("CONNECT");
-		return -1;
-	}
-	else
-		printf ("Conexion con el Planificador lograda\n");
-
-	int C2 = connect(socketMemoria, serverInfo_memoria->ai_addr, serverInfo_memoria->ai_addrlen);
-	if (C2==-1){
-		perror ("CONNECT");
-		return -1;
-	}
-	else
-		printf ("Conexion con la Memoria lograda\n");
-
-
-	freeaddrinfo(serverInfo_planificador);
-	freeaddrinfo(serverInfo_memoria);
-*/
-
-	int socketPlanificador = crearClientePlani(IP, PUERTOPLANIFICADOR);
-	int socketMemoria = crearClienteMem(IP_MEMORIA, PUERTOMEMORIA);
+	int socketPlanificador = crearClientePlani(IP, PUERTOPLANIFICADOR); //conecta con el planificador
+	int socketMemoria = crearClienteMem(IP_MEMORIA, PUERTOMEMORIA);//conecta con la memoria
 
 //Recepcion de instrucciones
 
