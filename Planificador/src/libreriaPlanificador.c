b648e5d4abc35499236d993d016397c788b80066
Gonzalo Alejo Latorre <gonzalatorre@users.noreply.github.com>
2015-10-09 16:58:50 -0300

    Ordene las funciones
    las limpie y comente un poco mas lo que tiene que hacer cada una. Cree la función dispatcher.

--- Planificador/src/libreriaPlanificador.c
+++ Planificador/src/libreriaPlanificador.c
@@ -26,76 +26,6 @@ void traigoContexto()
  miContexto.algoritmoPlanificacion = config_get_string_value(config_planificador, "ALGORITMO_PLANIFICACION" );
 }
 
-//Funcion que permite procesar el PCB creado a partir del comando correr PATH
-void procesarPCB(t_queue* cola_ready, char * path, int socketCPU)
-{
-	CPUenUso=true;
-	/*enviar(PCB) a la CPU
-	 *esperar a que la CPU termine de procesar
-	 *recibe respuesta de la CPU loguea y termina
-	 */
-
-	//inicializo las variables y reservo memoria (hay que ver donde hacer los free)
-	t_msj msj;
-	t_pcb* pcb = malloc(sizeof(t_pcb));
-	pcb->ruta = malloc(20);
-
-	//header
-	msj.headMSJ.tipo_ejecucion = 1;
-	//tamaño a recibir igual tamaño total del msj menos el header que lo recibe de una
-	msj.headMSJ.tamanio_msj = (sizeof(t_msj) - sizeof(t_headcpu));
-
-	//copio el path
-	int x;
-	for (x = 0; x < 20; x++) {
-		msj.ruta[x] = path[x];
-		pcb->ruta[x] = path[x];
-	}
-
-	//armo PCB y msj para enviar al CPU
-	pcb->instructionPointer = 0;
-	msj.instructionPointer = pcb->instructionPointer;
-	pcb->numInstrucciones = 0;
-	msj.numInstrucciones = pcb->numInstrucciones;
-	pcb->prioridad=0;
-	msj.prioridad = pcb->prioridad;
-	pcb->permisos=0;
-	msj.permisos = pcb->permisos;
-	pcb->PID = max_PID +1;
-	msj.PID = pcb->PID;
-	max_PID = pcb->PID; // ¿aca no habria q preguntar si es menor primero?? (lucho)
-
-	/*ENVIO DE MSJS A LA CPU
-	 *	pcb->PID=contador;
-	 *	pcb->instructionPointer=0
-	 *	pcb->numInstrucciones=0;
-	 *	pcb->prioridad=0;
-	 *	pcb->permisos=0;
-	 *	pcb->ruta=(char*)malloc(sizeof(path));
-	 *	pcb.ruta=path;
-	 *	free(path);
-	 *	send(socketCPU, pcb, sizeof(t_pcb));
-	 */
-	/*RECIBE MSJS LA CPU
-	 * 	t_pcb pcb=(t_pcb*)malloc(sizeof(t_pcb));
-	 * 	recibo LA DIRECCION DE MEMORIA del pcb
-	 * 	pcb=msj;
-	 * 	imprimo a ver que onda
-	 */
-
-	//creo q aca estaria bueno q pregunte por el algoritmo de planificacion configurado ¿?(lucho)
-	//encolo a la cola de ready
-	//queue_push(cola_ready, &pcb);
-	//le envio a la CPU el t_headcpu y el PCB
-
-	CPUenUso=false;
-
-	send(socketCPU, &msj,sizeof(t_msj),0);
-	sem_post(semProduccionMsjs);
-	puts("Mensaje enviado\n");
-	log_info(logger,"Comienzo ejecucion PID: %d Nombre: %s", pcb->PID, pcb->ruta);
-}
-
 
 void iniciarPlanificador(t_queue* cola_ready)
 {
@@ -128,8 +58,6 @@ void iniciarPlanificador(t_queue* cola_ready)
 		//Permite el envio de paquetes, dependiendo si la opcion elegida es valida
 		printf("COMANDOS VALIDOS:\n"
 			   "'correr PATH':      Para correr un proceso en el CPU (la primera por defecto)\n"
-			   "'cpu':              Para cambiar de CPU\n"
-			   "'menu':             Para volver al menu principal\n");
 		while(enviar)
 		{
 			fflush(stdin);
@@ -141,16 +69,6 @@ void iniciarPlanificador(t_queue* cola_ready)
 				break;
 				printf("Comando no implementado");
 			}
-			//MODIFICAR QUE PASA SI LA CPU ESTA EN USO
-			/*
-			if(CPUenUso)
-			{
-				//creo el hilo con la funcion procesarPCB (PCB);
-				if(pthread_create(&hCPU1,NULL,procesarPCB,&PCB)<0)
-					perror("Error HILO CPU!");
-			}
-			*/
-
 			/*
 			 * Protocolo de mensajes Planificador -CPU
 			 * Para poder entender los distintos tipos de mensajes que se envia, mandamos primero
@@ -181,3 +99,84 @@ void iniciarPlanificador(t_queue* cola_ready)
 		}
 	}
 }
+
+//Funcion despachador, que sacar un proceso de la cola de ready y enviarlo a la CPU para su ejecucion.
+void dispatcher(t_pcb *cola_ready, int CPU)
+{
+	//queue_pop(t_queue *cola_ready); //quita el primer elemento de la cola a la CPU disponible
+	//send(cola_ready, CPU); //envia el primer elemento de la cola
+	
+}
+
+//Funcion que permite procesar el PCB creado a partir del comando correr PATH
+t_pcb* procesarPCB(char * path)
+{
+	CPUenUso=true;
+	 /*esperar a que la CPU termine de procesar
+	 *recibe respuesta de la CPU loguea y termina
+	 */
+
+	//inicializo las variables y reservo memoria (hay que ver donde hacer los free)
+	//t_msj msj;
+	t_pcb* pcb = malloc(sizeof(t_pcb));
+	pcb->ruta = malloc(20);//MODIFICAR
+
+	//header
+	msj.headMSJ.tipo_ejecucion = 1;
+	//tamaño a recibir igual tamaño total del msj menos el header que lo recibe de una
+	msj.headMSJ.tamanio_msj = (sizeof(t_msj) - sizeof(t_headcpu));
+
+	//copio el path
+	int x;
+	for (x = 0; x < 20; x++) {
+		//msj.ruta[x] = path[x];
+		pcb->ruta[x] = path[x];
+	}
+
+	//armo PCB y msj para enviar al CPU
+	pcb->PID = max_PID +1;
+	pcb->instructionPointer = 0;
+	pcb->numInstrucciones = 0;
+	pcb->prioridad=0;
+	pcb->permisos=0;
+	//strcpy(pcb->ruta, path);
+	
+	
+	//Comento esto, ya que esta funcion solo va a asignar los valores al PCB no enviarlos.
+	/*
+	msj.instructionPointer = pcb->instructionPointer;
+	msj.numInstrucciones = pcb->numInstrucciones;
+	msj.prioridad = pcb->prioridad;
+	msj.permisos = pcb->permisos;
+	msj.PID = pcb->PID;
+	*/
+	
+	/*ENVIO DE MSJS A LA CPU
+	 *	pcb->PID=contador;
+	 *	pcb->instructionPointer=0
+	 *	pcb->numInstrucciones=0;
+	 *	pcb->prioridad=0;
+	 *	pcb->permisos=0;
+	 *	pcb->ruta=(char*)malloc(sizeof(path));
+	 *	pcb.ruta=path;
+	 *	free(path);
+	 *	send(socketCPU, pcb, sizeof(t_pcb));
+	 */
+	/*RECIBE MSJS LA CPU
+	 * 	t_pcb pcb=(t_pcb*)malloc(sizeof(t_pcb));
+	 * 	recibo LA DIRECCION DE MEMORIA del pcb
+	 * 	pcb=msj;
+	 * 	imprimo a ver que onda
+	 */
+
+	CPUenUso=false;
+	
+	return pcb;
+	/*esto lo tiene que hacer el despachador
+	send(socketCPU, &msj,sizeof(t_msj),0);
+	sem_post(semProduccionMsjs);
+	puts("Mensaje enviado\n");
+	log_info(logger,"Comienzo ejecucion PID: %d Nombre: %s", pcb->PID, pcb->ruta);
+	*/
+}
+
