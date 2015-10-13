33c2cddbfc0d6ca933ea1b47a8dfada4de72a0e4
Gonzalo Alejo Latorre <gonzalatorre@users.noreply.github.com>
2015-10-13 16:21:51 -0300

    "limpie" la funcion iniciarPlanificador
    y agregue cosas a la función dispatcher

--- Planificador/src/libreriaPlanificador.c
+++ Planificador/src/libreriaPlanificador.c
@@ -29,122 +29,87 @@ void traigoContexto()
 
 void iniciarPlanificador(t_queue* cola_ready)
 {
-	int primeraVez = 1;
-	int enviar =1 ;
-	int cpuElegida;
-	int socketCPU = conexiones.CPU[0];
-	char* message=malloc(20);
-	//t_pcb *PCB=(t_pcb*)malloc(sizeof(t_pcb));
-	//PCB->ruta=path;
-	// encolar(inicio, PCB);
-	//controla que el usuario no quiera salir
-	while(strcmp(message,"menu\n") !=0)
+	//int primeraVez = 1;
+	//int cpuElegida;
+	/*
+	if(!primeraVez)
 	{
-		if(!primeraVez)
+		//Muestra las conexiones con las CPUS disponibles
+		puts("Elija CPU: ¡¡¡SOLO NUMEROS!!!\n");
+		int j = 0;
+		while ( j < 1)
 		{
-			//Muestra las conexiones con las CPUS disponibles
-			puts("Elija CPU: ¡¡¡SOLO NUMEROS!!!\n");
-			int j = 0;
-			while ( j < 1)
-			{
-				printf("CPU n°:%d, puerto: %d\n",j+1,conexiones.CPU[j]);
-				j++;
-			}
-			//Permite elegir la conexion con el CPU deseado
-			scanf("%d", &cpuElegida);
-			socketCPU = conexiones.CPU[cpuElegida];
+			printf("CPU n°:%d, puerto: %d\n",j+1,conexiones.CPU[j]);
+			j++;
 		}
+		//Permite elegir la conexion con el CPU deseado
+		scanf("%d", &cpuElegida);
+		socketCPU = conexiones.CPU[cpuElegida];
+	}
+	*/	
+	//primeraVez=1;
+	
+	puts("entre a correr");
+	//pido la ruta del archivo
+	puts("Ingrese el nombre del archivo que desea correr:\n");
+	fflush(stdin);
+	fgest(ruta, 20, stdin);
+	//imprimo tamaño de la ruta ingresada
+	printf("%d", sizeof(ruta));
+	//char *path = (char*)malloc(sizeof(string_substring_from(message, 7)));
+	//Se crea el PCB y se lo pone en la cola de ready
+	queue_push(cola_ready, procesarPCB(ruta));
+	puts("PCB procesado y encolado\n");
+	sem_post(&semConsola); // debe ir arriba del procesarPCB para que se aproveche el paralelismo
 
-		//Permite el envio de paquetes, dependiendo si la opcion elegida es valida
-		printf("COMANDOS VALIDOS:\n"
-			   "'correr PATH':      Para correr un proceso en el CPU (la primera por defecto)\n"
-			   "'menu':				Para volver al menú anterior\n");
-
-		while(enviar)
-		{
-			fflush(stdin);
-			fgets(message, 20, stdin);			// Lee una linea en el stdin (lo que escribimos en la consola) hasta encontrar un \n (y lo incluye) o llegar a PACKAGESIZE.
-			printf("muestra lo q obtubo fgets en message: %s", message);
-			if (!strcmp(message,"cpu\n"))
-			{
-				primeraVez = 0;
-				//enviar = 0;			// Chequeo que el usuario no quiera salir
-				break;
-				printf("Comando no implementado");
-			}
-			/*
-			 * Protocolo de mensajes Planificador -CPU
-			 * Para poder entender los distintos tipos de mensajes que se envia, mandamos primero
-			 * un header t_headcpu.
-			 * tipo_ejecucion: 0 - salir
-			 * 				   1 - correr programa
-			 * 				   	   envia PCB
-			 * tamanio_mensaje: tamanio del char * o del PCB
-			 */
-			if (!strcmp(string_substring(message,0,7),"correr "))
-			{
-				puts("entre a correr");
-				primeraVez=1;
-				char *path = (char*)malloc(sizeof(string_substring_from(message, 7)));
-				//Tomo el path
-				path = string_substring_from(message, 7); //inclye el \n
-				printf("muestro la var path antes de mandarla para ver si no esta mandando basura: %s", path);
-
-				puts("PCB Procesado\n");
-				sem_post(&semConsola); // debe ir arriba del procesarPCB para que se aproveche el paralelismo
-
-				//PROCESO EL PCB
-				//procesarPCB(cola_ready, path,socketCPU);
-
-				//sem_wait(&semSalir); //es para pruebas
-				free(path);
-				break;
-			}
-			if (!strcmp(string_substring(message,0,5),"menu\n"))
-			{
-				//Salgo
-				sem_post(&semConsola);
-				enviar = 0;
-				break;
-			}
-		}
+	//sem_wait(&semSalir); //es para pruebas
+	free(path);
+	break;
 	}
 	//puts("ya sali del while");//prueba
 }
 
 //Funcion despachador, que sacar un proceso de la cola de ready y enviarlo a la CPU para su ejecucion.
-void dispatcher(t_pcb *cola_ready, int CPU)
+void dispatcher(t_pcb *cola_ready)
 {
+	/*
+	 * Protocolo de mensajes Planificador -CPU
+	 * Para poder entender los distintos tipos de mensajes que se envia, mandamos primero
+	 * un header t_headcpu.
+	 * tipo_ejecucion: 0 - salir
+	 *  1 - correr programa
+	 * 				   envia PCB
+	 * tamanio_mensaje: tamanio del char * o del PCB
+	 */
+	//busca la primer CPU que no este en uso
+	/*
+	while(!CPUenUso[I])
+	{
+		I++;
+	}
+	//¿Si no hay CPU disponible?
+	*/
+	//send(cola_ready, conexiones.CPU[I]); //envia el primer elemento de la cola
+	CPUenUSO[I]=false; //pone la CPU ocupada hasta que reciba los resultados de la CPU.
+	//ver en donde pongo la CPU como no ucupada.
 	//queue_pop(t_queue *cola_ready); //quita el primer elemento de la cola a la CPU disponible
-	//send(cola_ready, CPU); //envia el primer elemento de la cola
 	
 }
 
 //Funcion que permite procesar el PCB creado a partir del comando correr PATH
-t_pcb* procesarPCB(char * path)
+t_pcb* procesarPCB(char *path)
 {
-	CPUenUso=true;
-	 /*esperar a que la CPU termine de procesar
-	 *recibe respuesta de la CPU loguea y termina
-	 */
-
 	//inicializo las variables y reservo memoria (hay que ver donde hacer los free)
 	//t_msj msj;
 	t_pcb* pcb = malloc(sizeof(t_pcb));
-	pcb->ruta = malloc(20);//MODIFICAR
+	pcb->ruta =(char*)malloc(sizeof(path);//MODIFICAR
 
 	//header
-
 	msj.headMSJ.tipo_ejecucion = 1;
 	//tamaño a recibir igual tamaño total del msj menos el header que lo recibe de una
 	msj.headMSJ.tamanio_msj = (sizeof(t_msj) - sizeof(t_headcpu));
 
 	//copio el path
-	int x;
-	for (x = 0; x < 20; x++) {
-		//msj.ruta[x] = path[x];
-		pcb->ruta[x] = path[x];
-	}
 
 	//armo PCB y msj para enviar al CPU
 	pcb->PID = max_PID +1;
@@ -152,7 +117,7 @@ t_pcb* procesarPCB(char * path)
 	pcb->numInstrucciones = 0;
 	pcb->prioridad=0;
 	pcb->permisos=0;
-	//strcpy(pcb->ruta, path);
+	strcpy(pcb->ruta, path);
 	
 	
 	//Comento esto, ya que esta funcion solo va a asignar los valores al PCB no enviarlos.
@@ -182,10 +147,10 @@ t_pcb* procesarPCB(char * path)
 	 * 	imprimo a ver que onda
 	 */
 
-	CPUenUso=false;
 	
 	return pcb;
-	/*esto lo tiene que hacer el despachador
+	/*
+	esto lo tiene que hacer el despachador
 	send(socketCPU, &msj,sizeof(t_msj),0);
 	sem_post(semProduccionMsjs);
 	puts("Mensaje enviado\n");
