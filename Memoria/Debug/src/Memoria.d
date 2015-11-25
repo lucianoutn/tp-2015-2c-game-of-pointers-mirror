2bd1b2a9df2d1f48221acdac49429701768f9ffa
Luciano Marques <luciano.utn@gmail.com>
2015-11-25 04:54:37 -0300

    agrego algunos .d

--- Memoria/Debug/src/Memoria.d
+++ Memoria/Debug/src/Memoria.d
@@ -1,21 +1,10 @@
 src/Memoria.d: ../src/Memoria.c \
- /home/utnso/git/tp-2015-2c-game-of-pointers/commons/commons/config.h \
- /home/utnso/git/tp-2015-2c-game-of-pointers/commons/commons/collections/dictionary.h \
- /home/utnso/git/tp-2015-2c-game-of-pointers/commons/commons/collections/node.h \
  /home/utnso/git/tp-2015-2c-game-of-pointers/SharedLibs/SharedLibs/libreriaCliente.h \
  /home/utnso/git/tp-2015-2c-game-of-pointers/SharedLibs/SharedLibs/libreriaServidor.h \
  /home/utnso/git/tp-2015-2c-game-of-pointers/SharedLibs/SharedLibs/comun.h \
- /home/utnso/git/tp-2015-2c-game-of-pointers/commons/commons/log.h \
  ../src/funcMemory.h \
  /home/utnso/git/tp-2015-2c-game-of-pointers/SharedLibs/SharedLibs/manejoListas.h \
- /home/utnso/git/tp-2015-2c-game-of-pointers/commons/commons/collections/list.h \
  /home/utnso/git/tp-2015-2c-game-of-pointers/SharedLibs/SharedLibs/comun.h
-
-/home/utnso/git/tp-2015-2c-game-of-pointers/commons/commons/config.h:
-
-/home/utnso/git/tp-2015-2c-game-of-pointers/commons/commons/collections/dictionary.h:
-
-/home/utnso/git/tp-2015-2c-game-of-pointers/commons/commons/collections/node.h:
 
 /home/utnso/git/tp-2015-2c-game-of-pointers/SharedLibs/SharedLibs/libreriaCliente.h:
 
@@ -23,12 +12,8 @@ src/Memoria.d: ../src/Memoria.c \
 
 /home/utnso/git/tp-2015-2c-game-of-pointers/SharedLibs/SharedLibs/comun.h:
 
-/home/utnso/git/tp-2015-2c-game-of-pointers/commons/commons/log.h:
-
 ../src/funcMemory.h:
 
 /home/utnso/git/tp-2015-2c-game-of-pointers/SharedLibs/SharedLibs/manejoListas.h:
-
-/home/utnso/git/tp-2015-2c-game-of-pointers/commons/commons/collections/list.h:
 
 /home/utnso/git/tp-2015-2c-game-of-pointers/SharedLibs/SharedLibs/comun.h:
--- Memoria/Debug/src/subdir.mk
+++ Memoria/Debug/src/subdir.mk
@@ -20,7 +20,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/git/tp-2015-2c-game-of-pointers/commons" -I"/home/utnso/git/tp-2015-2c-game-of-pointers/SharedLibs" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I"/home/utnso/git/so-commons-library" -I"/home/utnso/git/tp-2015-2c-game-of-pointers/SharedLibs" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
